8359e7e413353ec37699c175742f49d6edc8129f
Tyler <tyler.starr413@gmail.com>
2022-07-28 08:16:10 -0600

    Updated port to use process.env

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ app.get('/', function(req, res){
     res.send('Hello world!')
 })
 
-var port = (process.argv.length > 2
-    && process.argv[2]
-    || 8000)
+const port = process.env.PORT || 3000
 app.listen(port, function(){
     console.log('Chaos-in-Camelot listening on port %s!', port)
 })
